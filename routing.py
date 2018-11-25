b39f521d54bf2572dc4f9f7a47fbd6a0904cb26d
Tudor Avram <a96tudor@gmail.com>
2018-11-25 14:37:48 +0200

    integrated with storage module

--- routing.py
+++ routing.py
@@ -5,58 +5,75 @@ from multiprocessing.pool import ThreadPool
 import subprocess
 from werkzeug.utils import secure_filename
 
+import storage.handler_user as handler_user
+
+_INNER_ID_DOCTOR = 'inner_id1'
+_INNER_ID_RADIOLOG  = 'inner_id2'
+_DUMMY_REQUEST_ID = 1
+
+handler = handler_user.StorageHandler('config.json')
+
 uploaded = False
 UPLOAD_FOLDER = 'i:/upload'
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-           
-data = [{'Nume':"Ion Ionescu", 'CNP':"1234567890", 'link':"http://example.com"}]
 
 @app.route('/doctor_page', methods=['GET'])
 def doctor_page():
-            return render_template('doctor_page.html', data=data )
+    data = handler.get_requests_for_doctor(_INNER_ID_DOCTOR)['requests']
+    return render_template('doctor_page.html', data=data)
+
 
 @app.route('/new_request', methods=['GET', 'POST'])
 def new_request():
     if request.method == 'POST':
-        pacient_name = request.form['name']
-        pacient_cnp = request.form['cnp']
-        print(pacient_cnp, pacient_name)
+        pacient_id = request.form['id']
+        comment = request.form['comment']
+        req_id = handler.create_request(_INNER_ID_DOCTOR, pacient_id, comment)['rid']
         return '''<!doctype html>
-                    <h1> Success here's your patient ID </h1>
-                    '''
+                    <h1> Success here's the request id: {}</h1>
+                    '''.format(req_id)
                     
     return render_template('newrequest.html')
 
+
 @app.route('/results', methods=['GET'])
 def results():
-            return render_template('results2.html')
+    results = handler.get_request(_DUMMY_REQUEST_ID)['results']
+    keys = list(results.keys())
+    data = {
+        'patologie{}'.format(i+1): results[keys[i]]['name']
+        for i in range(len(keys))
+    }
+    data.update({
+        'scor{}'.format(i+1): results[keys[i]]['yes']
+        for i in range(len(keys))
+    })
+    return render_template('results2.html', data=data)
+
 
 @app.route('/radiologie', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
-        pacient_id = request.form['id']
-        pacient_comment = request.form['comment']
-        print(pacient_comment, pacient_id)
-        # check if the post request has the file part
+        request_id = request.form['id']
         if 'file' not in request.files:
-            flash('No file part')
             return redirect(request.url)
         file = request.files['file']
         # if user does not select file, browser also
         # submit a empty part without filename
         if file.filename == '':
-            flash('No selected file')
             return redirect(request.url)
         if file and allowed_file(file.filename):
             file_bytes = file.read()
-            print(file_bytes)
+
+        handler.upload_photo(_INNER_ID_RADIOLOG, request_id, file_bytes)
         return redirect('/')
 
     return render_template('radiologist_page.html')
